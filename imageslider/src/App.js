69745079d91cc69b530318b9f717aab22b94b6a8
farhanpro <shaikhfarhan549@gmail.com>
2023-09-15 17:47:22 +0530

    Completed

---
+++ imageslider/src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+import ImageSlider from './ImageSlider';
+import { SliderData } from './SliderData';
+
+function App() {
+  return <ImageSlider slides={SliderData} />;
+}
+
+export default App;
